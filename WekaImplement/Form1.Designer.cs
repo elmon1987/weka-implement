82c9893cec93e17c4abab69eec869d9b47994fe7
Walpurisnacht <walpurisnacht.klarissa@gmail.com>
2015-09-30 00:51:27 +0700

    Weka Implement v1.0 [Changelogs] - Can get info of attributes from .csv files - Organize dataset (need PR)

---
+++ WekaImplement/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace WekaImplement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.F_Path = new System.Windows.Forms.TextBox();
+            this.F_Load = new System.Windows.Forms.Button();
+            this.F_Save = new System.Windows.Forms.Button();
+            this.F_Data = new System.Windows.Forms.TextBox();
+            this.Debug_Chk = new System.Windows.Forms.CheckBox();
+            this.I_Info = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // F_Path
+            // 
+            this.F_Path.Location = new System.Drawing.Point(13, 13);
+            this.F_Path.Name = "F_Path";
+            this.F_Path.ReadOnly = true;
+            this.F_Path.Size = new System.Drawing.Size(629, 20);
+            this.F_Path.TabIndex = 0;
+            // 
+            // F_Load
+            // 
+            this.F_Load.Location = new System.Drawing.Point(648, 13);
+            this.F_Load.Name = "F_Load";
+            this.F_Load.Size = new System.Drawing.Size(59, 21);
+            this.F_Load.TabIndex = 1;
+            this.F_Load.Text = "Load";
+            this.F_Load.UseVisualStyleBackColor = true;
+            this.F_Load.Click += new System.EventHandler(this.F_Load_Click);
+            // 
+            // F_Save
+            // 
+            this.F_Save.Location = new System.Drawing.Point(713, 12);
+            this.F_Save.Name = "F_Save";
+            this.F_Save.Size = new System.Drawing.Size(59, 21);
+            this.F_Save.TabIndex = 2;
+            this.F_Save.Text = "Save";
+            this.F_Save.UseVisualStyleBackColor = true;
+            this.F_Save.Click += new System.EventHandler(this.F_Save_Click);
+            // 
+            // F_Data
+            // 
+            this.F_Data.Location = new System.Drawing.Point(13, 129);
+            this.F_Data.Multiline = true;
+            this.F_Data.Name = "F_Data";
+            this.F_Data.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.F_Data.Size = new System.Drawing.Size(759, 420);
+            this.F_Data.TabIndex = 3;
+            this.F_Data.WordWrap = false;
+            // 
+            // Debug_Chk
+            // 
+            this.Debug_Chk.AutoSize = true;
+            this.Debug_Chk.Location = new System.Drawing.Point(648, 40);
+            this.Debug_Chk.Name = "Debug_Chk";
+            this.Debug_Chk.Size = new System.Drawing.Size(58, 17);
+            this.Debug_Chk.TabIndex = 4;
+            this.Debug_Chk.Text = "Debug";
+            this.Debug_Chk.UseVisualStyleBackColor = true;
+            // 
+            // I_Info
+            // 
+            this.I_Info.Location = new System.Drawing.Point(13, 40);
+            this.I_Info.Name = "I_Info";
+            this.I_Info.Size = new System.Drawing.Size(75, 23);
+            this.I_Info.TabIndex = 5;
+            this.I_Info.Text = "Show Info";
+            this.I_Info.UseVisualStyleBackColor = true;
+            this.I_Info.Click += new System.EventHandler(this.I_Info_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.I_Info);
+            this.Controls.Add(this.Debug_Chk);
+            this.Controls.Add(this.F_Data);
+            this.Controls.Add(this.F_Save);
+            this.Controls.Add(this.F_Load);
+            this.Controls.Add(this.F_Path);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "Form1";
+            this.Text = "Weka Implement";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox F_Path;
+        private System.Windows.Forms.Button F_Load;
+        private System.Windows.Forms.Button F_Save;
+        private System.Windows.Forms.TextBox F_Data;
+        private System.Windows.Forms.CheckBox Debug_Chk;
+        private System.Windows.Forms.Button I_Info;
+    }
+    public class Instances
+    {
+        public string Attribute { get; set; }
+        public string Type { get; set; }
+    }
+    public class DataSet
+    {
+        public System.Collections.Generic.List<Instances> Info { get; set; }
+        public System.Collections.Generic.List<object[]> Data { get; set; }
+    }
+}
+
---
+++ WekaImplement/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WekaImplement
+{
+    public partial class Form1 : Form
+    {
+        private string instances;
+        private DataSet dataset = new DataSet();
+        public Form1()
+        {
+            InitializeComponent();
+            dataset.Info = new List<Instances>();
+            dataset.Data = new List<object[]>();
+        }
+
+        private void DataProcess(string[] lines) //value line
+        {
+            object[,] predata = new object[lines.Length-1, lines[0].Split(',').Length];
+
+            //Debug
+            if (Debug_Chk.Checked)
+            {
+                MessageBox.Show(predata.GetLength(0).ToString() + " " + predata.GetLength(1).ToString(), "Dimension Debug");
+                this.Text = "Debug Mode";
+            }
+
+            for (int i = 1; i < lines.Length; i++)
+            {
+                string[] line = lines[i].Split(','); //split value into string array
+                
+                double d_tmp;
+                
+                for (int j = 0; j < line.Length; j++)
+                {
+                    if (dataset.Info[j].Type == "Numerical")
+                    {
+                        if (double.TryParse(line[j], out d_tmp))
+                            predata[i - 1, j] = d_tmp;
+                        else
+                            predata[i - 1, j] = "?";
+                    }
+                    else if (dataset.Info[j].Type == "Nominal")
+                        predata[i - 1, j] = line[j];
+                }
+            }
+
+            //Debug
+            if (Debug_Chk.Checked)
+            {
+                string _res = "";
+                for (int i = 0; i < predata.GetLength(0); i++)
+                {
+                    for (int j = 0; j < predata.GetLength(1); j++)
+                    {
+                        _res += predata[i, j] + " ";
+                    }
+                    _res += "\n";
+                }
+                MessageBox.Show(_res, "Data Debug");
+            }
+
+            for (int i = 0; i < predata.GetLength(0); i++) //i=2 j=12
+            {
+                object[] o_data = new object[predata.GetLength(1)];
+                for (int j = 0; j < predata.GetLength(1); j++)
+                {
+                    o_data[j] = predata[i, j];
+                }
+                dataset.Data.Add(o_data);
+            }
+
+            //Debug
+            if (Debug_Chk.Checked)
+            {
+                string _res = "";
+                foreach (object[] o_data in dataset.Data)
+                {
+                    foreach (object k in o_data)
+                        _res += k.ToString() + " ";
+                    _res += "\n";
+                }
+                MessageBox.Show(_res, "Dataset Data Debug");
+            }
+            this.Text = "Weka Implement";
+        }
+        private void Reset()
+        {
+            F_Data.Clear();
+            F_Path.Clear();
+            instances = "";
+            dataset.Info.Clear();
+            dataset.Data.Clear();
+        }
+        private void F_Load_Click(object sender, EventArgs e)
+        {
+            Reset();
+            OpenFileDialog open = new OpenFileDialog();
+
+            open.Filter = "CSV file(*.csv)|*.csv";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                F_Path.Text = open.FileName;
+                string[] lines = File.ReadAllLines(open.FileName);
+
+                foreach (string line in lines)
+                {
+                    instances += line + "\r\n"; //save data into instances
+                }
+
+                //Debug
+                if (Debug_Chk.Checked)
+                {
+                    F_Data.Text = instances; //test load
+                    this.Text = "Debug Mode";
+                }
+                else this.Text = "Weka Implement";
+
+                I_Info.PerformClick();
+                DataProcess(lines);
+
+                MessageBox.Show("Load success!", "Notification"); 
+            }          
+        }
+
+        private void F_Save_Click(object sender, EventArgs e)
+        {
+            if (F_Data.Text == "")  MessageBox.Show("Nothing to save!", "Notification");
+            else
+            {
+                SaveFileDialog save = new SaveFileDialog();
+
+                save.Filter = "CSV file(*.csv)|*.csv";
+
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    File.WriteAllText(save.FileName, F_Data.Text);
+                }
+
+                MessageBox.Show("File saved!", "Notification");
+
+                //Debug
+                if (Debug_Chk.Checked) //test save data
+                {
+                    Reset();
+                    this.Text = "Debug Mode";
+                    string[] lines = File.ReadAllLines(save.FileName);
+                    foreach (string line in lines)
+                    {
+                        instances += line + "\r\n";
+                    }
+                    MessageBox.Show(instances);
+                }
+                else this.Text = "Weka Implement";
+            }
+        }
+
+        private void I_Info_Click(object sender, EventArgs e)
+        {
+            if (dataset.Info.Count != 0) dataset.Info.Clear();
+            if (instances == "" || F_Path.Text == "") MessageBox.Show("Data not found!", "Notification");
+            else
+            {
+                var data = File.ReadLines(F_Path.Text).Take(2);
+                string[] s_tmp = new string[2];
+
+                //Convert var to string[]
+                int j = 0;
+                {
+                    foreach(var s in data)
+                    {
+                        s_tmp[j] = s.ToString();
+                        j++;
+                    }
+                }
+
+                //Debug
+                if (Debug_Chk.Checked)
+                {
+                    this.Text = "Debug Mode";
+                    MessageBox.Show(s_tmp[0] + " " + s_tmp[1],"var Debug");
+                }
+
+                string attrib = s_tmp[0];
+                string cmpr = s_tmp[1];
+
+                //Debug
+                if (Debug_Chk.Checked)
+                {
+                    MessageBox.Show(attrib + "\n" + cmpr,"attrib + cmpr");
+                }
+
+                attrib = attrib.Replace("\"", "");
+
+                string[] s_attrib = attrib.Split(',');
+                string[] s_cmpt = cmpr.Split(',');
+
+                string[] s_type = new string[] { "Numerical", "Nominal" };
+
+                attrib = "";
+                double _tmp;
+
+                //Process attribute
+                for (int i = 0; i < s_attrib.Length; i++)
+                {
+                    attrib += "Attribute: " + s_attrib[i] + "\tType: ";
+                    if (s_cmpt[i] == "?" || double.TryParse(s_cmpt[i], out _tmp))
+                    {
+                        attrib += s_type[0] + "\n";
+                        dataset.Info.Add(new Instances { Attribute = s_attrib[i], Type = s_type[0] });
+                    }
+                    else
+                    {
+                        attrib += s_type[1] + "\n";
+                        dataset.Info.Add(new Instances { Attribute = s_attrib[i], Type = s_type[1] });
+                    }
+                }
+                MessageBox.Show(attrib,"Attribute Info");
+
+                //Debug
+                if (Debug_Chk.Checked)
+                {
+                    string _res = "";
+                    foreach (Instances x in dataset.Info)
+                    {
+                        _res += x.Attribute + " " + x.Type + "\n";
+                    }
+                    MessageBox.Show(_res, "Instances Debug");
+                }
+                this.Text = "Weka Implement";
+            }
+        }
+    }
+}
